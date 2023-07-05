6e0c041a6b90c2b5ec8829e7f96e6ca052de82de
pramod-wdo <pramodhc007@gmail.com>
2023-07-05 07:21:30 +0530

    adds routes users page as landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :tests
-  root to: 'tests#index'
+  root to: 'users#index'
   get 'home/index'
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
